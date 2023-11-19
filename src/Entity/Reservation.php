6a33a7f67bcf1c4be4f909923d0098d97e4af719
Mevin Redureau <mel.redureau@gmail.com>
2023-11-19 11:09:53 +0100

    :construction: BDD update, those files weren't in precedent commit.

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -6,47 +6,97 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ReservationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
 #[ApiResource]
 class Reservation
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\CustomIdGenerator("doctrine.uuid_generator")]
+    private ?Uuid $id = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $dateHeure = null;
+    private ?\DateTimeInterface $startDate = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $statut = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
 
-    public function getId(): ?int
+    #[ORM\Column]
+    private ?\DateTimeImmutable $creaeAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $userId = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Listing $listingId = null;
+
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
 
-    public function getDateHeure(): ?\DateTimeInterface
+    public function getStartDate(): ?\DateTimeInterface
     {
-        return $this->dateHeure;
+        return $this->startDate;
     }
 
-    public function setDateHeure(\DateTimeInterface $dateHeure): static
+    public function setStartDate(\DateTimeInterface $startDate): static
     {
-        $this->dateHeure = $dateHeure;
+        $this->startDate = $startDate;
 
         return $this;
     }
 
-    public function getStatut(): ?string
+    public function getEndDate(): ?\DateTimeInterface
     {
-        return $this->statut;
+        return $this->endDate;
     }
 
-    public function setStatut(string $statut): static
+    public function setEndDate(\DateTimeInterface $endDate): static
     {
-        $this->statut = $statut;
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getCreaeAt(): ?\DateTimeImmutable
+    {
+        return $this->creaeAt;
+    }
+
+    public function setCreaeAt(\DateTimeImmutable $creaeAt): static
+    {
+        $this->creaeAt = $creaeAt;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(?User $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getListingId(): ?Listing
+    {
+        return $this->listingId;
+    }
+
+    public function setListingId(?Listing $listingId): static
+    {
+        $this->listingId = $listingId;
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,7 +4,15 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints\Date;
+use function Symfony\Component\Clock\now;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -12,35 +20,64 @@ use Doctrine\ORM\Mapping as ORM;
 class User
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\CustomIdGenerator("doctrine.uuid_generator")]
+    private ?Uuid $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $nom = null;
+    #[ORM\Column(length: 125)]
+    private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 125)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $emailVerified = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $role = null;
+    private ?string $hashedPassword = null;
 
-    public function getId(): ?int
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\OneToMany(mappedBy: 'userId', targetEntity: Listing::class, orphanRemoval: true)]
+    private Collection $listings;
+
+    #[ORM\OneToMany(mappedBy: 'userId', targetEntity: Account::class, orphanRemoval: true)]
+    private Collection $accounts;
+
+    #[ORM\OneToMany(mappedBy: 'userId', targetEntity: Reservation::class, orphanRemoval: true)]
+    private Collection $reservations;
+
+    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    private ?array $favoriteIds = null;
+
+    public function __construct()
+    {
+        $this->listings = new ArrayCollection();
+        $this->accounts = new ArrayCollection();
+        $this->reservations = new ArrayCollection();
+    }
+
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
 
-    public function getNom(): ?string
+    public function getName(): ?string
     {
-        return $this->nom;
+        return $this->name;
     }
 
-    public function setNom(string $nom): static
+    public function setName(string $name): static
     {
-        $this->nom = $nom;
+        $this->name = $name;
 
         return $this;
     }
@@ -57,26 +94,171 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    public function getEmailVerified(): ?\DateTimeInterface
     {
-        return $this->password;
+        return $this->emailVerified;
     }
 
-    public function setPassword(string $password): static
+    public function setEmailVerified(?\DateTimeInterface $emailVerified): static
     {
-        $this->password = $password;
+        $this->emailVerified = $emailVerified;
 
         return $this;
     }
 
-    public function getRole(): ?string
+    public function getImage(): ?string
     {
-        return $this->role;
+        return $this->image;
     }
 
-    public function setRole(string $role): static
+    public function setImage(?string $image): static
     {
-        $this->role = $role;
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getHashedPassword(): ?string
+    {
+        return $this->hashedPassword;
+    }
+
+    public function setHashedPassword(string $hashedPassword): static
+    {
+        $this->hashedPassword = $hashedPassword;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function setCreatedAtDefault(): static
+    {
+        $this->createdAt = now();
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Listing>
+     */
+    public function getListings(): Collection
+    {
+        return $this->listings;
+    }
+
+    public function addListing(Listing $listing): static
+    {
+        if (!$this->listings->contains($listing)) {
+            $this->listings->add($listing);
+            $listing->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeListing(Listing $listing): static
+    {
+        if ($this->listings->removeElement($listing)) {
+            // set the owning side to null (unless already changed)
+            if ($listing->getUserId() === $this) {
+                $listing->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Account>
+     */
+    public function getAccounts(): Collection
+    {
+        return $this->accounts;
+    }
+
+    public function addAccount(Account $account): static
+    {
+        if (!$this->accounts->contains($account)) {
+            $this->accounts->add($account);
+            $account->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAccount(Account $account): static
+    {
+        if ($this->accounts->removeElement($account)) {
+            // set the owning side to null (unless already changed)
+            if ($account->getUserId() === $this) {
+                $account->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Reservation>
+     */
+    public function getReservations(): Collection
+    {
+        return $this->reservations;
+    }
+
+    public function addReservation(Reservation $reservation): static
+    {
+        if (!$this->reservations->contains($reservation)) {
+            $this->reservations->add($reservation);
+            $reservation->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReservation(Reservation $reservation): static
+    {
+        if ($this->reservations->removeElement($reservation)) {
+            // set the owning side to null (unless already changed)
+            if ($reservation->getUserId() === $this) {
+                $reservation->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getFavoriteIds(): ?array
+    {
+        return $this->favoriteIds;
+    }
+
+    public function setFavoriteIds(?array $favoriteIds): static
+    {
+        $this->favoriteIds = $favoriteIds;
 
         return $this;
     }
